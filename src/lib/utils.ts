328244f221f5a590b0046774f5b44825539b3692
Aarón J. Montes <32050436+ajomuch92@users.noreply.github.com>
2024-02-01 14:40:19 -0600

    fix: 🐛 Fixing ternary condition to accept different types

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,3 @@
-export function ternary<T>(condition: boolean, trulyVal: unknown, falsyVal: unknown): T {
+export function ternary<T>(condition: boolean|string|number|undefined|null, trulyVal: unknown, falsyVal: unknown): T {
     return (condition ? trulyVal : falsyVal) as T;
 }
