b4676b943b8cd0a3d931d93783bf5b920ffa90d4
Aarón J. Montes <32050436+ajomuch92@users.noreply.github.com>
2024-02-01 18:30:48 -0600

    feat: 🎸 Adding method to compare 2 dats

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,7 @@
 export function ternary<T>(condition: boolean|string|number|undefined|null, trulyVal: unknown, falsyVal: unknown): T {
     return (condition ? trulyVal : falsyVal) as T;
 }
+
+export function isSameDate(date1?: Date, date2?: Date) {
+    return date1?.getDate() === date2?.getDate() && date1?.getMonth() === date2?.getMonth() && date1?.getFullYear() === date2?.getFullYear();
+}
