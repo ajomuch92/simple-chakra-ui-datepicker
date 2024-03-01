0c6de563f87665b5fd5c1044a5bbfda815fee63c
Aarón J. Montes <32050436+ajomuch92@users.noreply.github.com>
2024-02-29 20:23:21 -0600

    feat: 🎸 Adding methods to compare dates

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,11 @@ export function ternary<T>(condition: boolean|string|number|undefined|null, trul
 export function isSameDate(date1?: Date, date2?: Date) {
     return date1?.getDate() === date2?.getDate() && date1?.getMonth() === date2?.getMonth() && date1?.getFullYear() === date2?.getFullYear();
 }
+
+export function isGreaterThanDate(date1?: Date, date2?: Date) {
+    return !!date1 && !!date2 && date1.getTime() > date2.getTime();
+}
+
+export function isLessThanDate(date1?: Date, date2?: Date) {
+    return !!date1 && !!date2 && date1.getTime() < date2.getTime();
+}
