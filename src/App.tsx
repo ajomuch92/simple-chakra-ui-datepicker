5477a726f8339fcb3ace317a56d813179e7eb71b
Aarón J. Montes <32050436+ajomuch92@users.noreply.github.com>
2024-03-31 09:45:37 -0600

    refactor: 💡 Updating example root file

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <main style={{width: '100%', height: '100dvh', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
       <h1>Example</h1>
-      <SimpleDatePicker maxDate={new Date(2024, 3, 16)} minDate={new Date(2020, 0, 1 )}/>
+      <SimpleDatePicker maxDate={new Date(2024, 3, 16)} minDate={new Date(2024, 0, 1 )} colorSchema='green' activeColor='green.400'/>
     </main>
   )
 }
